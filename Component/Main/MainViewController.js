2fd63732345cda4f190ece778167501e3fbe281f
SaiDicaprio <hejiawei@lvmama.com>
2018-02-01 15:13:22 +0800

    TabBar和Navigator封装抽取

--- Component/Main/MainViewController.js
+++ Component/Main/MainViewController.js
@@ -30,79 +30,41 @@ export default class MainViewController extends Component {
     render(){
         return(
             <TabNavigator>
-                {/*首页*/}
-                <TabNavigator.Item
-                    title='首页'
-                    renderIcon={() => <Image source={{uri:'icon_tabbar_homepage'}} style={styles.iconStyle}/>}
-                    renderSelectedIcon={() => <Image source={{uri:'icon_tabbar_homepage_selected'}} style={styles.iconStyle}/>}
-                    onPress={() => {this.setState({selectedTab:'home'})} }
-                    selected={this.state.selectedTab === 'home'} >
-                    <Navigator
-                        initialRoute={{name:'home', component:HomeViewController}}
-                        configureScene={()=>{
-                            return Navigator.SceneConfigs.PushFromRight;
-                        }}
-                        renderScene={(route,navigator) => {
-                            let  Component = route.component;
-                            return <Component {...route.passProps} navigator={navigator}/>
-                        }}
-                    />
-                </TabNavigator.Item>
-                {/*商家*/}
-                <TabNavigator.Item
-                    title='商家'
-                    renderIcon={() => <Image source={{uri: 'icon_tabbar_merchant_normal'}} style={styles.iconStyle}/>} // 图标
-                    renderSelectedIcon={() =><Image source={{uri: 'icon_tabbar_merchant_selected'}} style={styles.iconStyle}/>}   // 选中的图标
-                    onPress={() => {this.setState({selectedTab:'shop'})} }
-                    selected={this.state.selectedTab === 'shop'}>
-                    <Navigator
-                        initialRoute={{name:'shop', component:ShopViewController}}
-                        configureScene={()=>{
-                            return Navigator.SceneConfigs.PushFromRight;
-                        }}
-                        renderScene={(route,navigator) => {
-                            let  Component = route.component;
-                            return <Component {...route.passProps} navigator={navigator}/>
-                        }}
-                    />
-                </TabNavigator.Item>
-                {/*我的*/}
-                <TabNavigator.Item
-                    title='我的'
-                    renderIcon={() => <Image source={{uri: 'icon_tabbar_mine'}} style={styles.iconStyle}/>} // 图标
-                    renderSelectedIcon={() =><Image source={{uri: 'icon_tabbar_mine_selected'}} style={styles.iconStyle}/>}   // 选中的图标
-                    onPress={() => {this.setState({selectedTab:'profile'})} }
-                    selected={this.state.selectedTab === 'profile'}>
-                    <Navigator
-                        initialRoute={{name:'profile', component:ProfileViewController}}
-                        configureScene={()=>{
-                            return Navigator.SceneConfigs.PushFromRight;
-                        }}
-                        renderScene={(route,navigator) => {
-                            let  Component = route.component;
-                            return <Component {...route.passProps} navigator={navigator}/>
-                        }}
-                    />
-                </TabNavigator.Item>
-                {/*更多*/}
-                <TabNavigator.Item
-                    title='更多'
-                    renderIcon={() => <Image source={{uri: 'icon_tabbar_misc'}} style={styles.iconStyle}/>} // 图标
-                    renderSelectedIcon={() =><Image source={{uri: 'icon_tabbar_misc_selected'}} style={styles.iconStyle}/>}   // 选中的图标
-                    onPress={() => {this.setState({selectedTab:'more'})} }
-                    selected={this.state.selectedTab === 'more'} >
-                    <Navigator
-                        initialRoute={{name:'more', component:MoreViewController}}
-                        configureScene={()=>{
-                            return Navigator.SceneConfigs.PushFromRight;
-                        }}
-                        renderScene={(route,navigator) => {
-                            let  Component = route.component;
-                            return <Component {...route.passProps} navigator={navigator}/>
-                        }}
-                    />
-                </TabNavigator.Item>
+                {/*--首页--*/}
+                {this.addChildViewController('首页', 'icon_tabbar_homepage', 'icon_tabbar_homepage_selected','home', HomeViewController)}
+                {/*--商家--*/}
+                {this.addChildViewController('商家', 'icon_tabbar_merchant_normal', 'icon_tabbar_merchant_selected','shop', ShopViewController)}
+                {/*--我的--*/}
+                {this.addChildViewController('我的', 'icon_tabbar_mine', 'icon_tabbar_mine_selected','mine', ProfileViewController)}
+                {/*--更多--*/}
+                {this.addChildViewController('更多', 'icon_tabbar_misc', 'icon_tabbar_misc_selected','more', MoreViewController)}
             </TabNavigator>
+        )
+    }
+
+    // 每一个TabBarItem
+    addChildViewController(title, iconName, selectedIconName, selectedTab, component, badgeText){
+        return(
+            <TabNavigator.Item
+                title={title}  // 传递变量,一定要加{}
+                renderIcon={() => <Image source={{uri: iconName}} style={styles.iconStyle}/>} // 图标
+                renderSelectedIcon={() =><Image source={{uri: selectedIconName}} style={styles.iconStyle}/>}   // 选中的图标
+                onPress={()=>{this.setState({selectedTab:selectedTab})}}
+                selected={this.state.selectedTab === selectedTab}
+                selectedTitleStyle={styles.selectedTitleStyle}
+                badgeText = {badgeText}
+            >
+                <Navigator
+                    initialRoute={{name:title,component:component}}
+                    configureScene={()=>{
+                        return Navigator.SceneConfigs.PushFromRight;
+                    }}
+                    renderScene={(route,navigator)=>{
+                        let Component = route.component;
+                        return <Component {...route.passProps} navigator={navigator}/>;
+                    }}
+                />
+            </TabNavigator.Item>
         )
     }
 }
@@ -111,6 +73,10 @@ const styles = StyleSheet.create({
     iconStyle:{
         width: Platform.OS === 'ios' ? 30 : 25,
         height:Platform.OS === 'ios' ? 30 : 25
+    },
+
+    selectedTitleStyle:{
+        color:'#ef5100'
     }
 });
 
