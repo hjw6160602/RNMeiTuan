bce18652dd14acbb49eb80ace3171fe3bd973752
SaiDicaprio <hejiawei@lvmama.com>
2018-02-01 16:37:56 +0800

    导航栏样式调整

--- App.js
+++ App.js
@@ -6,18 +6,10 @@
 
 import React, { Component } from 'react';
 import {
-    Platform ,
     StyleSheet,
     View
 } from 'react-native';
 import MainViewController from "./Component/Main/MainViewController";
-
-const instructions = Platform.select({
-    ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-    android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
 
 export default class App extends Component<{}> {
     render() {
@@ -29,12 +21,10 @@ export default class App extends Component<{}> {
     }
 }
 
-
 const styles = StyleSheet.create({
     // 整体的样式
     container: {
         flex:1,
         backgroundColor: '#F5FCFF'
     }
-
 });
